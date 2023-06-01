fcba8eccb9bd73eb089d6bd091b0381422c3b593
viktorsalivan <125485903+viktorsalivan@users.noreply.github.com>
2023-06-01 23:45:21 +0300

    SystemMainManager
    C#

---
+++ SystemMainManager/Controllers/Controller.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SystemMainManager.Controllers
+{
+   public class Controller
+    {
+        public void memorycompressionDisable()
+        {
+            Process pc1 = Process.Start(new ProcessStartInfo
+            {
+                FileName = "powershell",
+                Arguments = "/command disable-mmagent -memorycompression",
+                CreateNoWindow = true,
+                UseShellExecute = false
+            }); //
+        } //memorycompressionFull()
+        public void pagecombiningDisable()
+        {
+            Process pc1 = Process.Start(new ProcessStartInfo
+            {
+                FileName = "powershell",
+                Arguments = "/command disable-mmagent -pagecombining",
+                CreateNoWindow = true,
+                UseShellExecute = false
+            }); //
+        }//pagecombiningFull
+
+        public void applicationprelaunchDisable()
+        {
+            Process pc1 = Process.Start(new ProcessStartInfo
+            {
+                FileName = "powershell",
+                Arguments = "/command disable-mmagent -applicationprelaunch",
+                CreateNoWindow = true,
+                UseShellExecute = false
+            }); //
+        }//applicationprelaunch
+
+        public void applicationlaunchprefetchingDisable()
+        {
+            Process pc1 = Process.Start(new ProcessStartInfo
+            {
+                FileName = "powershell",
+                Arguments = "/command disable-mmagent -applicationLaunchPrefetching",
+                CreateNoWindow = true,
+                UseShellExecute = false
+            }); //
+        }//applicationprelaunch
+
+        public void operationapiDisable()
+        {
+            Process pc1 = Process.Start(new ProcessStartInfo
+            {
+                FileName = "powershell",
+                Arguments = "/command disable-mmagent -operationapi",
+                CreateNoWindow = true,
+                UseShellExecute = false
+            }); //
+        }
+
+ 
+    } //class Controller
+
+} //END
---
+++ SystemMainManager/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SystemMainManager.Controllers;
+
+namespace SystemMainManager
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Controller _controller;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _controller = new Controller();
+        }
+
+        private void command_exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btStart_command(object sender, RoutedEventArgs e)
+        {
+            if (cbApplicationprelaunch.IsChecked == true)
+            {
+                _controller.applicationprelaunchDisable();
+                cbApplicationprelaunch.IsEnabled = false;
+            } //if
+            
+            if (cbApplicationlaunchprefetching.IsChecked == true)
+            {
+                _controller.applicationlaunchprefetchingDisable();
+                cbApplicationlaunchprefetching.IsEnabled = false;
+            } // if
+            if (cbMemorycompression.IsChecked == true)
+            {
+                _controller.memorycompressionDisable();
+                cbMemorycompression.IsEnabled = false;
+            }
+            if (cbPagecombining.IsChecked == true)
+            {
+                _controller.pagecombiningDisable();
+                cbPagecombining.IsEnabled = false;
+            }
+            if (cbOperationapi.IsChecked == true)
+            {
+                _controller.operationapiDisable();
+                cbOperationapi.IsEnabled = false;
+            }
+
+        }//btStart_command
+    }
+}
